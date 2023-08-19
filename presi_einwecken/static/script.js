f68924426b41822e819ac8db1c054b89d6cb4586
timtombobjohn <derchefkoch98@gmail.com>
2023-08-19 11:26:02 +0200

    v190823

--- presi_einwecken/static/script.js
+++ presi_einwecken/static/script.js
@@ -12,7 +12,8 @@ function sleep(ms) {
 
 var imageSources = [
     ["/static/bilder/film1/img01.png", "/static/bilder/film1/img02.png", "/static/bilder/film1/img03.png","/static/bilder/film1/img04.png","/static/bilder/film1/img05.png","/static/bilder/film1/img06.png"],
-    ["/static/bilder/film2/img01.png", "/static/bilder/film2/img02.png", "/static/bilder/film2/img03.png","/static/bilder/film2/img04.png","/static/bilder/film2/img05.png","/static/bilder/film2/img06.png"]
+    ["/static/bilder/film2/img01.png", "/static/bilder/film2/img02.png", "/static/bilder/film2/img03.png","/static/bilder/film2/img04.png","/static/bilder/film2/img05.png","/static/bilder/film2/img06.png"],
+    ["/static/bilder/film3/img01.png", "/static/bilder/film3/img02.png", "/static/bilder/film3/img03.png","/static/bilder/film3/img04.png","/static/bilder/film3/img05.png","/static/bilder/film3/img06.png"]
 ];
 
 var currentIndex = 1;
@@ -20,27 +21,34 @@ var currentIndex = 1;
 async function changeImage() {
   const img = document.getElementsByClassName("slide-img");
 
-  for(let i = 0; i<img.length; i++){
+ 
+   for(let i = 0; i<img.length; i++){
+    console.log("i: ",i, "\n img: ",img,"\n currentIndex: ", currentIndex, "\n imageSources[i][currentIndex]", imageSources[i][currentIndex])
     img[i].style.opacity=0;
-    await sleep(3000);
+    await sleep(3200);
+    
     img[i].src = imageSources[i][currentIndex];
     img[i].style.opacity=1;
-    await sleep(7000);
+    await sleep(4000);
   }
+ 
+ //alte überblendung für nur zwei videos:
+ // for(let i = 0; i<img.length+1; i++){
+//    img[i].style.opacity=0;
+  //  await sleep(3000);
+  //  img[i].src = imageSources[i][currentIndex];
+  //  img[i].style.opacity=1;
+ //   await sleep(7000);
+ // }
 
   // await sleep(2000);
 
-  // for(let i = 0; i<img.length; i++){
-  //   img[i].src = imageSources[i][currentIndex];
-  //   img[i].style.opacity=1;
-  //   await sleep(400);
-  // }
 
   currentIndex++;
   currentIndex = currentIndex % 6
 }
 
-setInterval(changeImage, 20000); 
+setInterval(changeImage, 21600); 
 
 
 
